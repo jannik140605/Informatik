beb84822226f0f2d66f205e54dc80216730c4ce9
jannik140605 <93092502+jannik140605@users.noreply.github.com>
2021-10-24 20:01:59 +0200

    Add files via upload

---
+++ Informatik /UNO_online_game_java/Variables.java
@@ -0,0 +1,18 @@
+/* Variable defining of the UNO game */
+
+public class Variables {
+
+	public static void method1(String[] args) {
+	
+		
+	
+	}
+	
+	public static void method2(String[] args) {
+	
+		
+	
+	}
+	
+	
+}	
