2ce1f9e8b1119d4c2937bff1d0c0af0134f9834f
jannik140605 <93092502+jannik140605@users.noreply.github.com>
2021-10-24 21:17:14 +0200

    Add files via upload

---
+++ Informatik/UNO/Main.java
@@ -0,0 +1,18 @@
+/* Main execution of the UNO game */
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		
+	
+	}
+	
+	public static void method1(String[] args) {
+	
+		
+	
+	}
+	
+	
+}	
---
+++ Informatik/UNO/window.java
@@ -0,0 +1,9 @@
+/* Create a window for our game */
+
+public class window {
+
+	public static void Window() {
+	
+		createCanvas(600, 600);	
+		background(51);
+	}
